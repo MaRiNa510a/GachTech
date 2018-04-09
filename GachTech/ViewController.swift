c84fe42000205d22ca4c71090d29b227f1165bfd
Marina Goto <5m.clover3@gmail.com>
2018-04-09 17:38:14 +0900

    Initial Commit

---
+++ GachTech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GachTech
+//
+//  Created by Marina Goto on 2018/04/09.
+//  Copyright © 2018年 Marina Goto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
